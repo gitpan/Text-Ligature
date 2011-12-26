dd5a3849d40eb6c81eeb9ce27b718fd16fced168
Nick Patch <patch@cpan.org>
2011-12-26 04:15:43 +0000

    Import of PATCH/Text-Ligature-0.02 from CPAN.
    gitpan-cpan-distribution: Text-Ligature
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         PATCH/Text-Ligature-0.02.tar.gz
    gitpan-cpan-author:       PATCH
    gitpan-cpan-maturity:     released

--- lib/Text/Ligature.pm
+++ lib/Text/Ligature.pm
@@ -7,9 +7,15 @@ use utf8;
 use parent 'Exporter';
 use Carp;
 
-our $VERSION     = '0.01';
-our @EXPORT_OK   = qw( to_ligature from_ligature );
+our $VERSION     = '0.02';
+our @EXPORT_OK   = qw< to_ligatures from_ligatures to_ligature from_ligature >;
 our %EXPORT_TAGS = ( all => \@EXPORT_OK );
+
+# TODO: remove to_ligature/from_ligature in next release
+*to_ligature   = \&to_ligatures;
+*from_ligature = \&from_ligatures;
+
+my @defaults = qw< ff fi fl ffi ffl >;
 
 my %lig_for = (
     ff  => 'ﬀ',
@@ -23,27 +29,27 @@ my %lig_for = (
 
 my %chars_for = reverse %lig_for;
 
-sub to_ligature {
+sub to_ligatures {
     my ($text) = @_;
 
     if (@_ != 1) {
-        carp 'to_ligature() expects one argument';
+        carp 'to_ligatures() expects one argument';
         return;
     }
 
     # longest token matching
-    for my $chars (sort { length $b <=> length $a } keys %lig_for) {
+    for my $chars (sort { length $b <=> length $a } @defaults) {
         $text =~ s/$chars/$lig_for{$chars}/g;
     }
 
     return $text;
 }
 
-sub from_ligature {
+sub from_ligatures {
     my ($text) = @_;
 
     if (@_ != 1) {
-        carp 'from_ligature() expects one argument';
+        carp 'from_ligatures() expects one argument';
         return;
     }
 
@@ -66,30 +72,40 @@ Text::Ligature - Replace sequences of characters with typographic ligatures
 
 =head1 VERSION
 
-This document describes Text::Ligature version 0.01.
+This document describes Text::Ligature version 0.02.
 
 =head1 SYNOPSIS
 
     use Text::Ligature qw( :all );
 
-    to_ligature('offloading floral offices refines effectiveness');
+    to_ligatures('offloading floral offices refines effectiveness');
     # returns: oﬄoading ﬂoral oﬃces reﬁnes eﬀectiveness
 
-    from_ligature('oﬄoading ﬂoral oﬃces reﬁnes eﬀectiveness');
+    from_ligatures('oﬄoading ﬂoral oﬃces reﬁnes eﬀectiveness');
     # returns: offloading floral offices refines effectiveness
 
 =head1 DESCRIPTION
 
 Replaces sequences of characters with corresponding typographic ligatures.
 
-    Ligature    Non-ligature
-    ﬀ           ff
-    ﬁ           fi
-    ﬂ           fl
-    ﬃ           ffi
-    ﬄ           ffl
-    ﬅ           ft
-    ﬆ           st
+    Characters  Ligature
+    ff          ﬀ
+    fi          ﬁ
+    fl          ﬂ
+    ffi         ﬃ
+    ffl         ﬄ
+
+=begin comment
+
+TODO: Support additional specified characters.
+
+Additional:
+
+    Characters  Ligature
+    ft          ﬅ
+    st          ﬆ
+
+=end comment
 
 This is an early release.  Specifying the ligatures to replace will be
 supported in a future version.
